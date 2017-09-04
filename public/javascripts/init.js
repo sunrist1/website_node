ae60c31e51bb21522a9e462b570f5011c1ce66b3
sunrist1 <294333575@qq.com>
2017-09-05 00:50:01 +0800

    fixed index layout bug

--- public/javascripts/init.js
+++ public/javascripts/init.js
@@ -24,9 +24,24 @@
     $(".dropdown-button").dropdown({
       hover:true
     });
-
     // 图片点击放大
     // $('.materialboxed').materialbox();
 
   }); // end of document ready
 })(jQuery); // end of jQuery name space
+
+// 获取url参数
+function GetRequest() { 
+  var url = location.search; //获取url中"?"符后的字串 
+  var theRequest = new Object(); 
+  if (url.indexOf("?") != -1) { 
+      var str = url.substr(1); 
+      var strs = str.split("&"); 
+      for(var i = 0; i < strs.length; i ++) { 
+          var pos = strs[i].indexOf("=");
+          // theRequest[strs[i].split("=")[0]]=decodeURI(strs[i].split("=")[1]); 
+          theRequest[strs[i].split("=")[0]]=decodeURI(strs[i].substr(pos+1)); 
+      } 
+  } 
+  return theRequest; 
+} 
